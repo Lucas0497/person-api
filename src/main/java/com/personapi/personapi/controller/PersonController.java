a1cba471342e680c0f371e7cc749d68c7dace25e
Lucas <la989895@gmail.com>
2021-09-05 14:42:37 -0300

    Add class Person, PersonRepository, Phone, PhoneType and refresh PersonController

--- src/main/java/com/personapi/personapi/controller/PersonController.java
+++ src/main/java/com/personapi/personapi/controller/PersonController.java
@@ -1,7 +1,11 @@
 package com.personapi.personapi.controller;
 
 
+import com.personapi.personapi.entity.Person;
+import com.personapi.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test Deploy na nuvem !";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public String createPerson(Person person){
+        personRepository.save(person);
+        return "API Test 1";
 
     }
 }
